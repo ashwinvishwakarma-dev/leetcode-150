af7b647536cc17a5da965295a85d9819cad3d979
Ashwin Vishwakarma <ashwinvishwakarma.dev@gmail.com>
2026-07-29 17:49:47 +0530

    Time: 2 ms (12.08%) | Memory: 43.1 MB (5.63%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,36 +1,31 @@
 class Solution {
-    public boolean wordPattern(String pattern, String s) {
-          HashMap<Character, String> map = new HashMap<>();
-        String[] str = s.split("\\s");
+  public boolean wordPattern(String pattern, String s) {
+    String[] str = s.split("\\s");
 
-        HashSet<Character> characterSet = new HashSet<>();
-        HashSet<String> stringSet = new HashSet<>();
-
-        if (pattern.length() != str.length) {
-            return false;
-        }
-
-        for (char c : pattern.toCharArray()) {
-            characterSet.add(c);
-        }
-        for (String string : str) {
-            stringSet.add(string);
-        }
-        if (characterSet.size() != stringSet.size()) {
-            return false;
-        }
-        for (int i = 0; i < str.length; i++) {
-
-            if (map.containsKey(pattern.charAt(i))) {
-                if (!str[i].equals(map.get(pattern.charAt(i)))) {
-                    return false;
-                }
-            } else {
-                map.put(pattern.charAt(i), str[i]);
-            }
-
-        }
-
-        return true;
+    if (pattern.length() != str.length) {
+      return false;
     }
+
+    HashMap<Character, String> charToWord = new HashMap<>();
+    HashMap<String, Character> wordToChar = new HashMap<>();
+
+    for (int i = 0; i < pattern.length(); i++) {
+      if (charToWord.containsKey(pattern.charAt(i))) {
+        if (!charToWord.get(pattern.charAt(i)).equals(str[i])) {
+          return false;
+        }
+      }
+      charToWord.put(pattern.charAt(i), str[i]);
+    }
+    for (int i = 0; i < pattern.length(); i++) {
+      if (wordToChar.containsKey(str[i])) {
+        if (!wordToChar.get(str[i]).equals(pattern.charAt(i))) {
+          return false;
+        }
+      }
+      wordToChar.put(str[i], pattern.charAt(i));
+    }
+
+    return true;
+  }
 }
