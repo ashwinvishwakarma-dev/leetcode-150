2f63ffa895a3034f7881757a73b89d1cb83b6808
Ashwin Vishwakarma <ashwinvishwakarma.dev@gmail.com>
2026-08-09 23:39:56 +0530

    Time: 4 ms (96.55%) | Memory: 44.9 MB (64.00%) - LeetSync

---
+++ 71-simplify-path/simplify-path.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String simplifyPath(String path) {
+
+        Deque<String> stack = new ArrayDeque<>();
+
+        String[] parts = path.split("/");
+
+        for (String part : parts) {
+
+            if (part.equals("") || part.equals(".")) {
+                continue;
+            }
+
+            if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.removeLast();
+                }
+            } else {
+                stack.addLast(part);
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return "/";
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        for (String dir : stack) {
+            ans.append("/");
+            ans.append(dir);
+        }
+
+        return ans.toString();
+    }
+} 
