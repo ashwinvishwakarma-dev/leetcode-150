69fd08acc80e7573fabff77edf1680c91d22002f
Ashwin Vishwakarma <ashwinvishwakarma.dev@gmail.com>
2026-08-10 22:48:28 +0530

    Time: 4 ms (96.63%) | Memory: 44.7 MB (87.43%) - LeetSync

--- 71-simplify-path/simplify-path.java
+++ 71-simplify-path/simplify-path.java
@@ -1,36 +1,33 @@
 class Solution {
     public String simplifyPath(String path) {
 
-        Deque<String> stack = new ArrayDeque<>();
-
-        String[] parts = path.split("/");
-
-        for (String part : parts) {
-
-            if (part.equals("") || part.equals(".")) {
-                continue;
-            }
-
-            if (part.equals("..")) {
-                if (!stack.isEmpty()) {
-                    stack.removeLast();
-                }
-            } else {
-                stack.addLast(part);
-            }
-        }
-
-        if (stack.isEmpty()) {
-            return "/";
-        }
-
-        StringBuilder ans = new StringBuilder();
-
-        for (String dir : stack) {
-            ans.append("/");
-            ans.append(dir);
-        }
-
-        return ans.toString();
+      Stack<String> stack = new Stack<>();
+			
+			String[] strArr = path.split("/");
+			
+			for (String str : strArr) {
+				if (str.isEmpty()||str.equals(".")) {
+					continue;
+				}
+				if (str.equals("..")) {
+					if (!stack.isEmpty()) {
+						stack.pop();
+					}
+				}else {
+					stack.push(str);
+				}
+			}
+			
+			if (stack.isEmpty()) {
+				return "/";
+			}
+			
+			StringBuilder ans = new StringBuilder();
+			
+			for (String s : stack) {
+				ans.append("/");
+				ans.append(s);
+			}
+			return ans.toString(); 
     }
 } 
