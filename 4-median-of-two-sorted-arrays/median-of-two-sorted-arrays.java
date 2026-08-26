0a77dc14438ca8ea658581504f8354929546eea9
Ashwin Vishwakarma <ashwinvishwakarma.dev@gmail.com>
2026-08-26 22:22:26 +0530

    Time: 1 ms (100.00%) | Memory: 48.7 MB (86.80%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -0,0 +1,43 @@
+class Solution {
+  public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+      if(nums1.length > nums2.length){
+        return findMedianSortedArrays(nums2,nums1);
+      }
+
+    int m = nums1.length;
+    int n = nums2.length;
+    int total = m + n;
+
+    int left = 0;
+    int right = m;
+
+    while (left <= right) {
+
+      int mid1 = left + (right - left) / 2;
+      int mid2 = total / 2 - mid1;
+
+      int left1 = (mid1 == 0) ? Integer.MIN_VALUE : (nums1[mid1 - 1]);
+      int left2 = (mid2 == 0) ? Integer.MIN_VALUE : (nums2[mid2 - 1]);
+
+      int right1 = (mid1 == m) ? Integer.MAX_VALUE : (nums1[mid1]);
+      int right2 = (mid2 == n) ? Integer.MAX_VALUE : (nums2[mid2]);
+
+      if (left1 <= right2 && left2 <= right1) {
+        if (total % 2 == 0) {
+          int median = Math.max(left1, left2) + Math.min(right1, right2);
+          return median / 2.0;
+        } else {
+          int median = Math.min(right1, right2);
+          return (double) median;
+        }
+      }
+      if (left1 > right2) {
+        right = mid1 - 1;
+      } else {
+        left = mid1 + 1;
+      }
+    }
+    return 0.0;
+  }
+}
