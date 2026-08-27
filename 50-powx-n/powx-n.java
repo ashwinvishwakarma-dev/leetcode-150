f8bf6b31f28aafbdc159d29c5bd2c0007cfbb7b4
Ashwin Vishwakarma <ashwinvishwakarma.dev@gmail.com>
2026-08-27 22:24:49 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (22.30%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,20 @@
+class Solution {
+  public double myPow(double x, int n) {
+    long power = n;
+    double result = 1;
+    if (n < 0) {
+      x = 1 / x;
+      power = -power;
+    }
+
+    while (power > 0) {
+      if (power % 2 == 1) {
+        result = result * x;
+      }
+      power = power / 2;
+      x = x * x;
+    }
+
+    return result;
+  }
+}
