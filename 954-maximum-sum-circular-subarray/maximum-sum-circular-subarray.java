6f3b7748e70df7f21d6696b37821878b8c917f18
Ashwin Vishwakarma <ashwinvishwakarma.dev@gmail.com>
2026-09-03 23:44:35 +0530

    Time: 10 ms (6.19%) | Memory: 51.2 MB (41.97%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
@@ -1,45 +1,48 @@
 class Solution {
-    public  int maxSubarraySumCircular(int[] nums) {
-		int maxSum = kadaneMax(nums);
-		int minSum = kadaneMin(nums);
-		int totalSum = 0;
-		
-		for (int i = 0; i < nums.length; i++) {
-			totalSum = totalSum + nums[i];
-		}
-		
-		if (maxSum < 0) {
-			return maxSum;
-		}
-		
-		int wrappingSum = totalSum - minSum;
-		
-		return Math.max(maxSum, wrappingSum);
-	}
-	
-	private  int kadaneMax(int[] nums) {
-		int maxSum = nums[0];
-		int currentSum = nums[0];
-		
-		for (int i = 1; i < nums.length; i++) {
-			int current = nums[i];
-			currentSum = currentSum + nums[i];
-			currentSum = Math.max(current, currentSum);
-			maxSum = Math.max(currentSum, maxSum);
-		}
-		return maxSum;
-	}
-	
-	private  int kadaneMin(int[] nums) {
-		int minSum = nums[0];
-		int currentSum = nums[0];
-		
-		for (int i = 1; i < nums.length; i++) {
-			int current = nums[i];
-			currentSum = currentSum + nums[i];
-			currentSum = Math.min(current, currentSum);
-			minSum = Math.min(currentSum, minSum);
-		}
-		return minSum;
-	}
+  public int maxSubarraySumCircular(int[] nums) {
+
+    int totalSum = 0;
+    int maxSum = kadaneMax(nums);
+    int minSum = kadaneMin(nums);
+
+    for (int i = 0; i < nums.length; i++) {
+      totalSum = totalSum + nums[i];
+    }
+
+    if (maxSum < 0) {
+      return maxSum;
+    }
+
+    int wrappingSum = totalSum - minSum;
+
+    return Math.max(maxSum, wrappingSum);
+  }
+
+  // This Give Normal Max Subarray Sun with Kadane
+  private int kadaneMax(int[] nums) {
+    int maxSum = nums[0];
+    int currentSum = nums[0];
+
+    for (int i = 1; i < nums.length; i++) {
+      int current = nums[i];
+      currentSum = currentSum + nums[i];
+      currentSum = Math.max(current, currentSum);
+      maxSum = Math.max(currentSum, maxSum);
+    }
+    return maxSum;
+  }
+
+  // This Give Normal Min Subarray Sum with Kadane
+  private int kadaneMin(int[] nums) {
+    int minSum = nums[0];
+    int currentSum = nums[0];
+
+    for (int i = 1; i < nums.length; i++) {
+      int current = nums[i];
+      currentSum = currentSum + nums[i];
+      currentSum = Math.min(current, currentSum);
+      minSum = Math.min(currentSum, minSum);
+    }
+    return minSum;
+  }
 }
