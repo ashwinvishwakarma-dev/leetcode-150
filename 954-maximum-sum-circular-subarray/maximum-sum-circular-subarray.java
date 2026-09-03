b09ee59cafa3a7c700a251997c51964025e848fc
Ashwin Vishwakarma <ashwinvishwakarma.dev@gmail.com>
2026-09-03 23:41:31 +0530

    Time: 10 ms (6.19%) | Memory: 51 MB (79.29%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
@@ -1,32 +1,45 @@
 class Solution {
-    public int maxSubarraySumCircular(int[] nums) {
-
-    int totalSum = 0;
-
-    int currentMax = 0;
-    int maxSum = Integer.MIN_VALUE;
-
-    int currentMin = 0;
-    int minSum = Integer.MAX_VALUE;
-
-    for (int num : nums) {
-
-        // Maximum subarray - Kadane
-        currentMax = Math.max(num, currentMax + num);
-        maxSum = Math.max(maxSum, currentMax);
-
-        // Minimum subarray - reverse Kadane
-        currentMin = Math.min(num, currentMin + num);
-        minSum = Math.min(minSum, currentMin);
-
-        totalSum += num;
-    }
-
-    // All numbers are negative
-    if (maxSum < 0) {
-        return maxSum;
-    }
-
-    return Math.max(maxSum, totalSum - minSum);
-}
+    public  int maxSubarraySumCircular(int[] nums) {
+		int maxSum = kadaneMax(nums);
+		int minSum = kadaneMin(nums);
+		int totalSum = 0;
+		
+		for (int i = 0; i < nums.length; i++) {
+			totalSum = totalSum + nums[i];
+		}
+		
+		if (maxSum < 0) {
+			return maxSum;
+		}
+		
+		int wrappingSum = totalSum - minSum;
+		
+		return Math.max(maxSum, wrappingSum);
+	}
+	
+	private  int kadaneMax(int[] nums) {
+		int maxSum = nums[0];
+		int currentSum = nums[0];
+		
+		for (int i = 1; i < nums.length; i++) {
+			int current = nums[i];
+			currentSum = currentSum + nums[i];
+			currentSum = Math.max(current, currentSum);
+			maxSum = Math.max(currentSum, maxSum);
+		}
+		return maxSum;
+	}
+	
+	private  int kadaneMin(int[] nums) {
+		int minSum = nums[0];
+		int currentSum = nums[0];
+		
+		for (int i = 1; i < nums.length; i++) {
+			int current = nums[i];
+			currentSum = currentSum + nums[i];
+			currentSum = Math.min(current, currentSum);
+			minSum = Math.min(currentSum, minSum);
+		}
+		return minSum;
+	}
 }
